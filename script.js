5b69babc1ba56d8fb261bfb0d6a4260a7ef8e79d
Talupula Naveen <stylistar05@gmail.com>
2025-07-09 23:07:34 +0530

    setup references and create an array

---
+++ script.js
@@ -0,0 +1,25 @@
+const giftsContainer = document.getElementById("gifts-container");
+const luckyBtn = document.getElementById("luckyBtn");
+
+const gifts = [
+  "₹100 Cash",
+  "Chocolates",
+  "₹500",
+  "Smartphone",
+  "Headphones",
+  "₹50 Cash",
+  "Watch",
+  "Teddy Bear",
+  "₹200 Cash",
+  "Perfume",
+  "Sunglasses",
+  "₹1000 Cash",
+  "Laptop Bag",
+  "Keyboard",
+  "₹250 Cash",
+  "Goldchain",
+  "Charger",
+  "₹300 Cash",
+  "₹750 Cash",
+  "Earbuds",
+];
